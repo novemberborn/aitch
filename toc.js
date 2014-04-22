139b5a5d2711ed7ccedd74124f37f85144e9ffc0
Mark Wubben <mark@novemberborn.net>
2014-04-22 16:15:51 +0100

    Generated documentation for 624caccbd351101296c46cb341b797a5f1773c33

---
+++ toc.js
@@ -0,0 +1,501 @@
+window.files = [
+{
+  "path": "index.html",
+  "originalName": "README.md",
+  "originalPath": "README.md",
+  "name": "index.html",
+  "title": "aitch",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "aitch",
+      "title": "aitch"
+    },
+    {
+      "level": 2,
+      "slug": "installation",
+      "title": "Installation"
+    },
+    {
+      "level": 2,
+      "slug": "usage",
+      "title": "Usage"
+    },
+    {
+      "level": 2,
+      "slug": "examples",
+      "title": "Examples"
+    }
+  ]
+},
+{
+  "path": "lib/BaseClient.js.html",
+  "originalName": "BaseClient.js",
+  "originalPath": "lib/BaseClient.js",
+  "name": "BaseClient.js.html",
+  "lang": "javascript",
+  "title": "BaseClient([options])",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "baseclient-options-",
+      "title": "BaseClient([options])"
+    },
+    {
+      "level": 2,
+      "slug": "options",
+      "title": "Options"
+    },
+    {
+      "level": 3,
+      "slug": "pathname",
+      "title": "pathname"
+    },
+    {
+      "level": 3,
+      "slug": "query",
+      "title": "query"
+    },
+    {
+      "level": 3,
+      "slug": "headers",
+      "title": "headers"
+    },
+    {
+      "level": 3,
+      "slug": "auth",
+      "title": "auth"
+    },
+    {
+      "level": 3,
+      "slug": "transport",
+      "title": "transport"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient-pathname",
+      "title": "BaseClient#pathname"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient-query",
+      "title": "BaseClient#query"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient-headers",
+      "title": "BaseClient#headers"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient-auth",
+      "title": "BaseClient#auth"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient-transport",
+      "title": "BaseClient#transport"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient-request",
+      "title": "BaseClient#Request"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient-response",
+      "title": "BaseClient#Response"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient-_normalizequery-query-",
+      "title": "BaseClient#_normalizeQuery(query)"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient-_normalizeheaders-headers-",
+      "title": "BaseClient#_normalizeHeaders(headers)"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient-_issuerequest-method-options-",
+      "title": "BaseClient#_issueRequest(method, [options])"
+    }
+  ]
+},
+{
+  "path": "lib/Client.js.html",
+  "originalName": "Client.js",
+  "originalPath": "lib/Client.js",
+  "name": "Client.js.html",
+  "lang": "javascript",
+  "title": "Client([options])",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "client-options-",
+      "title": "Client([options])"
+    },
+    {
+      "level": 2,
+      "slug": "client-request-method-options-",
+      "title": "Client#request(method, [options])"
+    },
+    {
+      "level": 2,
+      "slug": "client-head-options-",
+      "title": "Client#head([options])"
+    },
+    {
+      "level": 2,
+      "slug": "client-get-options-",
+      "title": "Client#get([options])"
+    },
+    {
+      "level": 2,
+      "slug": "client-delete-options-",
+      "title": "Client#delete([options])"
+    },
+    {
+      "level": 2,
+      "slug": "client-put-options-",
+      "title": "Client#put([options])"
+    },
+    {
+      "level": 2,
+      "slug": "client-post-options-",
+      "title": "Client#post([options])"
+    },
+    {
+      "level": 2,
+      "slug": "client-patch-options-",
+      "title": "Client#patch([options])"
+    }
+  ]
+},
+{
+  "path": "lib/Request.js.html",
+  "originalName": "Request.js",
+  "originalPath": "lib/Request.js",
+  "name": "Request.js.html",
+  "lang": "javascript",
+  "title": "Request(client, method, options)",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "request-client-method-options-",
+      "title": "Request(client, method, options)"
+    },
+    {
+      "level": 2,
+      "slug": "options",
+      "title": "Options"
+    },
+    {
+      "level": 3,
+      "slug": "pathname",
+      "title": "pathname"
+    },
+    {
+      "level": 3,
+      "slug": "query",
+      "title": "query"
+    },
+    {
+      "level": 3,
+      "slug": "headers",
+      "title": "headers"
+    },
+    {
+      "level": 3,
+      "slug": "auth",
+      "title": "auth"
+    },
+    {
+      "level": 3,
+      "slug": "body-options",
+      "title": "Body options"
+    },
+    {
+      "level": 4,
+      "slug": "stream",
+      "title": "stream"
+    },
+    {
+      "level": 4,
+      "slug": "chunk",
+      "title": "chunk"
+    },
+    {
+      "level": 4,
+      "slug": "json",
+      "title": "json"
+    },
+    {
+      "level": 4,
+      "slug": "form",
+      "title": "form"
+    },
+    {
+      "level": 2,
+      "slug": "request-method",
+      "title": "Request#method"
+    },
+    {
+      "level": 2,
+      "slug": "request-path",
+      "title": "Request#path"
+    },
+    {
+      "level": 2,
+      "slug": "request-headers",
+      "title": "Request#headers"
+    },
+    {
+      "level": 2,
+      "slug": "request-body",
+      "title": "Request#body"
+    },
+    {
+      "level": 2,
+      "slug": "request-auth",
+      "title": "Request#auth"
+    },
+    {
+      "level": 2,
+      "slug": "request-default_json_content_type",
+      "title": "Request#DEFAULT_JSON_CONTENT_TYPE"
+    },
+    {
+      "level": 2,
+      "slug": "request-default_form_content_type",
+      "title": "Request#DEFAULT_FORM_CONTENT_TYPE"
+    },
+    {
+      "level": 2,
+      "slug": "request-buildpath-defaultpathname-basequery-options-",
+      "title": "Request#buildPath(defaultPathname, baseQuery, options)"
+    },
+    {
+      "level": 2,
+      "slug": "request-buildquerypairs-base-query-",
+      "title": "Request#buildQueryPairs(base, [query])"
+    },
+    {
+      "level": 2,
+      "slug": "request-buildheaders-base-options-",
+      "title": "Request#buildHeaders(base, options)"
+    },
+    {
+      "level": 2,
+      "slug": "request-buildbody-options-",
+      "title": "Request#buildBody(options)"
+    },
+    {
+      "level": 2,
+      "slug": "request-buildauth-auth-options-",
+      "title": "Request#buildAuth(auth, options)"
+    }
+  ]
+},
+{
+  "path": "lib/Response.js.html",
+  "originalName": "Response.js",
+  "originalPath": "lib/Response.js",
+  "name": "Response.js.html",
+  "lang": "javascript",
+  "title": "Response(executor)",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "response-executor-",
+      "title": "Response(executor)"
+    },
+    {
+      "level": 2,
+      "slug": "response-statuscode",
+      "title": "Response#statusCode"
+    },
+    {
+      "level": 2,
+      "slug": "response-headers",
+      "title": "Response#headers"
+    },
+    {
+      "level": 2,
+      "slug": "response-stream",
+      "title": "Response#stream"
+    }
+  ]
+},
+{
+  "path": "lib/Transport.js.html",
+  "originalName": "Transport.js",
+  "originalPath": "lib/Transport.js",
+  "name": "Transport.js.html",
+  "lang": "javascript",
+  "title": "Transport([options])",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "transport-options-",
+      "title": "Transport([options])"
+    },
+    {
+      "level": 2,
+      "slug": "transport-port",
+      "title": "Transport#port"
+    },
+    {
+      "level": 2,
+      "slug": "additional-options",
+      "title": "Additional options"
+    },
+    {
+      "level": 3,
+      "slug": "tls",
+      "title": "tls"
+    },
+    {
+      "level": 2,
+      "slug": "transport-_makerequest-options-",
+      "title": "Transport#_makeRequest(options)"
+    }
+  ]
+},
+{
+  "path": "lib/UnencryptedTransport.js.html",
+  "originalName": "UnencryptedTransport.js",
+  "originalPath": "lib/UnencryptedTransport.js",
+  "name": "UnencryptedTransport.js.html",
+  "lang": "javascript",
+  "title": "UnencryptedTransport([options])",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "unencryptedtransport-options-",
+      "title": "UnencryptedTransport([options])"
+    },
+    {
+      "level": 2,
+      "slug": "options",
+      "title": "Options"
+    },
+    {
+      "level": 3,
+      "slug": "hostname",
+      "title": "hostname"
+    },
+    {
+      "level": 3,
+      "slug": "port",
+      "title": "port"
+    },
+    {
+      "level": 3,
+      "slug": "agent",
+      "title": "agent"
+    },
+    {
+      "level": 2,
+      "slug": "unencryptedtransport-hostname",
+      "title": "UnencryptedTransport#hostname"
+    },
+    {
+      "level": 2,
+      "slug": "unencryptedtransport-port",
+      "title": "UnencryptedTransport#port"
+    },
+    {
+      "level": 2,
+      "slug": "unencryptedtransport-agent",
+      "title": "UnencryptedTransport#agent"
+    },
+    {
+      "level": 2,
+      "slug": "unencryptedtransport-_makerequest-options-",
+      "title": "UnencryptedTransport#_makeRequest(options)"
+    },
+    {
+      "level": 2,
+      "slug": "unencryptedtransport-issuerequest-descriptor-response-",
+      "title": "UnencryptedTransport#issueRequest(descriptor, [Response])"
+    }
+  ]
+},
+{
+  "path": "lib/main.js.html",
+  "originalName": "main.js",
+  "originalPath": "lib/main.js",
+  "name": "main.js.html",
+  "lang": "javascript",
+  "title": "main",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "main",
+      "title": "main"
+    },
+    {
+      "level": 2,
+      "slug": "baseclient",
+      "title": "BaseClient"
+    },
+    {
+      "level": 2,
+      "slug": "client",
+      "title": "Client"
+    },
+    {
+      "level": 2,
+      "slug": "request",
+      "title": "Request"
+    },
+    {
+      "level": 2,
+      "slug": "response",
+      "title": "Response"
+    },
+    {
+      "level": 2,
+      "slug": "transport",
+      "title": "Transport"
+    },
+    {
+      "level": 2,
+      "slug": "unencryptedtransport",
+      "title": "UnencryptedTransport"
+    },
+    {
+      "level": 2,
+      "slug": "validationhelpers",
+      "title": "validationHelpers"
+    }
+  ]
+},
+{
+  "path": "lib/validation-helpers.js.html",
+  "originalName": "validation-helpers.js",
+  "originalPath": "lib/validation-helpers.js",
+  "name": "validation-helpers.js.html",
+  "lang": "javascript",
+  "title": "Validation Helpers",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "validation-helpers",
+      "title": "Validation Helpers"
+    },
+    {
+      "level": 2,
+      "slug": "castheadervalue-value-header-inarray-",
+      "title": "castHeaderValue(value, header, inArray)"
+    },
+    {
+      "level": 2,
+      "slug": "castqueryvalue-value-param-",
+      "title": "castQueryValue(value, param)"
+    }
+  ]
+}
+];
